0948fa06d28235d340fc1b0bc5839b2f73cfc6f0
sg-moomin <rooney9325@naver.com>
2021-12-05 23:46:20 +0900

    flask 공부를 django로 변경

---
+++ 1. project/2. django/winRateProject/mysite/winRate/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WinrateConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'winRate'
---
+++ 1. project/2. django/winRateProjectInit.cmd
@@ -0,0 +1,3 @@
+@echo off
+cd c:/projects/winRateProject/mysite
+c:/venvs/winRateProject/scripts/activate
